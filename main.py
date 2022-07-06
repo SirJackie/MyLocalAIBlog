0fffe49c235b856dca4f72f33b8d8c9ecf8f5672
lyj00912 <3122742760@qq.com>
2022-07-06 20:39:37 +0800

    created the error calclating stuffs

--- main.py
+++ main.py
@@ -2,21 +2,23 @@ def myFunction(x):
     return 2*x**2 + x + 3
 
 
-def getGradient(myFunction, x, dx=0.02):
+def getGradient(myFunction, x, dx=0.001):
     dy = myFunction(x + dx) - myFunction(x)
     return dy / dx
 
 
 currentX = 9.8
-learningRate = 0.01
+learningRate = 0.0001
 resultX = []
 resultG = []
 
-for i in range(5000):
+for i in range(500000):
     resultX.append(currentX)
     gradient = getGradient(myFunction, currentX)
     resultG.append(gradient)
     currentX -= gradient * learningRate
 
-print(resultX.pop())
-print(resultG.pop())
+finalX = resultX.pop()
+finalY = myFunction(finalX)
+error = finalY - myFunction(-0.25)
+print(finalX, finalY, error)
