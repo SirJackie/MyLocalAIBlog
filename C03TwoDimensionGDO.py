a7d821750112910288df098530d1d199e4703083
lyj00912 <3122742760@qq.com>
2023-03-11 22:26:56 +0800

    created the GetGradient in C03TwoDimensionGDO

--- C03TwoDimensionGDO.py
+++ C03TwoDimensionGDO.py
@@ -20,7 +20,21 @@ def GetYDirectionDerivative(function, fixedX, y, dy=0.001):
     return dz / dy
 
 
-# print(MyFunction(2, 4))
-# print(MyFunction(-10, -10))
-print(GetXDirectionDerivative(MyFunction, -10, -10))
-print(GetYDirectionDerivative(MyFunction, -10, -10))
+def GetGradient(function, x, y):
+    xDerivative = GetXDirectionDerivative(MyFunction, x, y)
+    yDerivative = GetYDirectionDerivative(MyFunction, x, y)
+    return xDerivative, yDerivative  # returns a tuple, format is (dx, dy)
+
+
+def TestGradientCalculators():
+    print(MyFunction(2, 4))
+    print(MyFunction(-10, -10))
+
+    print(GetXDirectionDerivative(MyFunction, -10, -10))
+    print(GetYDirectionDerivative(MyFunction, -10, -10))
+
+    print(GetGradient(MyFunction, -10, -10))
+
+
+if __name__ == "__main__":
+    TestGradientCalculators()
