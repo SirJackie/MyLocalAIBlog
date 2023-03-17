c36bd814ec84631885b0a507611249904cad739e
lyj00912 <3122742760@qq.com>
2023-03-17 22:23:04 +0800

    optimized the code in C03TwoDimensionGDO

--- C03TwoDimensionGDO.py
+++ C03TwoDimensionGDO.py
@@ -25,37 +25,50 @@ def GetGradient(function, x, y):
 
 
 def TestGradientCalculators():
-    print(MyFunction(2, 4))
+    print(MyFunction(0, 0))
     print(MyFunction(-10, -10))
-
-    print(GetXDirectionDerivative(MyFunction, -10, -10))
-    print(GetYDirectionDerivative(MyFunction, -10, -10))
-
+    print(GetGradient(MyFunction, 0, 0))
     print(GetGradient(MyFunction, -10, -10))
 
 
-if __name__ == "__main__":
-    print(GetGradient(MyFunction, 0, 0))
-
-
 if __name__ == "__main_":
-    expectedX = 2
-    expectedY = 4
+    TestGradientCalculators()
+
+
+if __name__ == "__main__":
+    # Store X and Y History
+    xHis = []
+    yHis = []
+
+    # Set Parameters
+    expectedX = 0
+    expectedY = 0
     expectedZ = MyFunction(expectedX, expectedY)  # 0.0
 
     currentX = -10
     currentY = -10
-    learningRate = 1
+    learningRate = 0.1
 
-    for i in range(0, 5000):
+    # Iterate
+    for i in range(0, 500):
+        xHis.append(currentX)
+        yHis.append(currentY)
+
         gradient = GetGradient(MyFunction, currentX, currentY)
-        print(currentX, currentY, gradient)
         currentX += -gradient[0] * learningRate
         currentY += -gradient[1] * learningRate
 
+    # Final Result
+    xHis.append(currentX)
+    yHis.append(currentY)
     finalX = currentX
     finalY = currentY
     finalZ = MyFunction(finalX, finalY)
+
+    # Output
     print("finalX: %.5f; expectedX: %.5f; errorX: %.5f" % (finalX, expectedX, abs(finalX - expectedX)))
     print("finalY: %.5f; expectedY: %.5f; errorY: %.5f" % (finalY, expectedY, abs(finalY - expectedY)))
     print("finalZ: %.5f; expectedZ: %.5f; errorZ: %.5f" % (finalZ, expectedZ, abs(finalZ - expectedZ)))
+
+    print(xHis)
+    print(yHis)
