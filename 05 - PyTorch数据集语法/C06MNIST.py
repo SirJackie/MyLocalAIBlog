6673cbf88ebf20186b4c030cb2cbeb41d535c9c5
lyj00912 <3122742760@qq.com>
2023-04-09 10:28:27 +0800

    created MNIST loader

---
+++ 05 - PyTorch数据集语法/C06MNIST.py
@@ -0,0 +1,17 @@
+import torch
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+# 定义数据预处理操作，将图像数据转换为张量并进行标准化
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+# 加载 MNIST 数据集
+train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+
+# 创建数据加载器
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
